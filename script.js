d2422fb181c4239e776851ede3b0e91d71ef4f1a
zaya nay <andinirahayuu27@gmail.com>
2026-05-22 21:39:20 +0700

    update

--- script.js
+++ script.js
@@ -9,3 +9,46 @@ window.addEventListener('scroll', () => {
   }
 
 });
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+
+  const updateCounter = () => {
+
+    const target = +counter.getAttribute('data-target');
+
+    const current = +counter.innerText;
+
+    const increment = target / 80;
+
+    if(current < target){
+
+      counter.innerText =
+      `${Math.ceil(current + increment)}`;
+
+      setTimeout(updateCounter,20);
+
+    }else{
+
+      counter.innerText = target;
+
+      if(target === 300){
+        counter.innerText = target + '+';
+      }
+
+      if(target === 98){
+        counter.innerText = target + '%';
+      }
+
+      if(target === 5){
+        counter.innerText = target + '+';
+      }
+
+    }
+
+  };
+
+  updateCounter();
+
+});
